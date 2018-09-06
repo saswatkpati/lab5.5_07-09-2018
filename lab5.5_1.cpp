1b5ba2e93eec65b4ace12f3952078748dfc50293
saswatkpati <42134195+saswatkpati@users.noreply.github.com>
2018-09-07 01:52:43 +0530

    Add files via upload

---
+++ lab5.5_1.cpp
@@ -0,0 +1,14 @@
+//include the library
+#include <iostream>
+using namespace std;
+
+//mention the function
+int main()
+	{
+	int n = 5;
+	for(int i = 0; i < 5 ; i ++)
+	cout << "*****" << endl ;
+	cout << "This is Square Star Pattern" << endl ;
+	return 0;
+	}
+
---
+++ lab5.5_2.cpp
@@ -0,0 +1,21 @@
+//include the library
+#include <iostream>
+using namespace std;
+
+//mention the function
+int main()
+	{
+	int n = 3;
+	//print the first line of stars
+	cout << "*****" << endl ;
+	//looping structure for spaces
+	for(int i = 0; i < 3 ; i ++)
+	{
+	cout << "*   *" << endl ;
+	}	
+	cout << "*****" << endl ;
+	cout << "This is Hollow Square Star Pattern" << endl;
+//end the program
+return 0;
+	}
+

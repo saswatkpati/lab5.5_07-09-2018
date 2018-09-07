2e376fff2e77eb5729a9797f2dd94b73489462f1
saswatkpati <42134195+saswatkpati@users.noreply.github.com>
2018-09-07 06:32:20 +0000

    Update lab5.5_1.cpp

--- lab5.5_1.cpp
+++ lab5.5_1.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 //mention the function
 int main()
-{
-	int n = 5;
-	for(int i = 0; i < 5 ; i ++)
-	cout << "*****" << endl ;
+{int n ;
+	cout << "Please input your desired side length:" << endl; 
+	//read the input value
+	cin >> n;
+	for(int i = 0; i < n ; i ++)
+	{
+		for (int j=0; j < n ; j++)
+		{
+			cout << "* " ;
+		}
+	cout << endl;
+	}
 	cout << "This is Square Star Pattern" << endl ;
 	return 0;
 }

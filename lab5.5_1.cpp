16ad3df7536847ed1cf4c13d23d53e61ac111538
saswatkpati <42134195+saswatkpati@users.noreply.github.com>
2018-09-07 06:04:38 +0000

    Update lab5.5_1.cpp

--- lab5.5_1.cpp
+++ lab5.5_1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 //mention the function
 int main()
-	{
+{
 	int n = 5;
 	for(int i = 0; i < 5 ; i ++)
 	cout << "*****" << endl ;
 	cout << "This is Square Star Pattern" << endl ;
 	return 0;
-	}
+}
 

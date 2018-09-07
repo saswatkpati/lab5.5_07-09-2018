7263747cbf486bff71afea77a84508e754cbdc62
saswatkpati <42134195+saswatkpati@users.noreply.github.com>
2018-09-08 00:33:15 +0530

    Add files via upload

---
+++ lab5.5_q8.cpp
@@ -0,0 +1,26 @@
+//include the library
+#include <iostream>
+using namespace std;
+
+//mention the function
+int main()
+{	
+	int size ;
+	//ask for input value
+	cout << "Please input your desired side length for printing a hollow rhombus star pattern:" << endl; 
+	//read the input value
+	cin >> size;
+	//running the loop structure
+	for(int i = 0; i < size ; i ++)
+	{
+		for (int j=0; j <= i ; j++)
+		{
+			cout << "* " ;
+		}
+	cout << endl;
+	}
+//end the program
+return 0;
+}
+
+
